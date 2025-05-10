09d1381175b2942b5f06b22a48d67ac284776279
Daffa Rinaldi <daffaxzy890@gmail.com>
2025-05-10 14:44:28 +0700

    Modified Blade Templating

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,17 +1,4 @@
-<x-header></x-header>
-    <div class="min-h-full">
-        <x-navbar></x-navbar>
-
-        <header class="bg-white shadow-sm">
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact Page</h1>
-            </div>
-        </header>
-        
-        <main>
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            
-            </div>
-        </main>
-    </div>
-<x-footer></x-footer>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+    <h1>Ini Halaman Home</h1>
+</x-layout>
