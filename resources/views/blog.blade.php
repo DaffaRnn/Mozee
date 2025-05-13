8c7eca22b1f38ca56a1d7e6ea328cec1166eedb2
Daffa Rinaldi <daffaxzy890@gmail.com>
2025-05-13 12:22:14 +0700

    Customize blog

--- resources/views/blog.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-    <x-slot:title>{{ $title }}</x-slot>
-    <h1>Ini Halaman Home</h1>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,19 @@ Route::get('/about', function () {
     return view('about', ["title" => "About"]);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ["title" => "Blog"]);
+Route::get('/posts', function () {
+    return view('posts', ["title" => "Blog", "posts" => [
+        [
+            'title' => 'Title Article 1',
+            'author' => 'Daffa Rinaldi',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic ducimus asperiores laboriosam odit enim aperiam ipsum repellendus quas, corporis, non voluptates, est vel nemo impedit dolorem quisquam at blanditiis dolor.'
+        ],
+        [
+            'title' => 'Title Article 2',
+            'author' => 'Daffa Rinaldi',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi et, exercitationem tempora reprehenderit quos corporis rem architecto ipsum tempore? Quasi dolore magnam fuga molestiae sit ratione dolorem ducimus ullam similique.'
+        ]
+    ]]);
 });
 
 Route::get('/contact', function () {
