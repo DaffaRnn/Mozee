47c05d453691cf80728456ac04500439fca2973e
Daffa Rinaldi <daffaxzy890@gmail.com>
2025-05-21 20:37:54 +0700

    make single post for detailed article

---
+++ e
@@ -0,0 +1,59 @@
+[33mcommit 8c7eca22b1f38ca56a1d7e6ea328cec1166eedb2[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: Daffa Rinaldi <daffaxzy890@gmail.com>
+Date:   Tue May 13 12:22:14 2025 +0700
+
+    Customize blog
+
+[33mcommit d6538ac7b100a6902da5f26f47fb349ca66cc851[m
+Author: Daffa Rinaldi <daffaxzy890@gmail.com>
+Date:   Sat May 10 20:02:40 2025 +0700
+
+    new update nav-link
+
+[33mcommit 0be3d22c2590a56b0db3d0e1c243361346d91c5d[m
+Author: Daffa Rinaldi <daffaxzy890@gmail.com>
+Date:   Sat May 10 15:08:58 2025 +0700
+
+    Modified activate link on nav
+
+[33mcommit 09d1381175b2942b5f06b22a48d67ac284776279[m
+Author: Daffa Rinaldi <daffaxzy890@gmail.com>
+Date:   Sat May 10 14:44:28 2025 +0700
+
+    Modified Blade Templating
+
+[33mcommit 7a4882b498113c26245e3136e2ef90b2a6a9f654[m
+Author: Daffa Rinaldi <daffaxzy890@gmail.com>
+Date:   Sat May 10 13:56:42 2025 +0700
+
+    Implementation Blade Templating Laravel to Views for Navbar, Header, and Footer
+
+[33mcommit 021b717da2c16c0aa1d09361464958f566175c5b[m
+Author: Daffa Rinaldi <daffaxzy890@gmail.com>
+Date:   Wed May 7 12:47:45 2025 +0700
+
+    Modified Nav Link
+
+[33mcommit 41be9360520104da4847e6245f337ed16d8d6069[m
+Author: Daffa Rinaldi <daffaxzy890@gmail.com>
+Date:   Sat May 3 17:32:22 2025 +0700
+
+    install node, tailwind framework, and alpine.js to this project
+
+[33mcommit bcb3874a992c11705cf0684b6423f8b57ee22dc4[m
+Author: Daffa Rinaldi <daffaxzy890@gmail.com>
+Date:   Wed Apr 30 10:40:25 2025 +0700
+
+    edited blog & contact page
+
+[33mcommit 33675944cc101e873e29b42cdb1206f63ab75e57[m
+Author: Daffa Rinaldi <daffaxzy890@gmail.com>
+Date:   Wed Apr 30 10:25:11 2025 +0700
+
+    a little bit changes
+
+[33mcommit a2d8b0b0079c921d0b587a73601b628c1ac1290c[m
+Author: Daffa Rinaldi <daffaxzy890@gmail.com>
+Date:   Tue Apr 29 12:40:24 2025 +0700
+
+    Rebuild Laravel Project
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,16 +14,46 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     return view('posts', ["title" => "Blog", "posts" => [
         [
+            'id' => 1,
+            'slug' => 'title-article-1',
             'title' => 'Title Article 1',
             'author' => 'Daffa Rinaldi',
             'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic ducimus asperiores laboriosam odit enim aperiam ipsum repellendus quas, corporis, non voluptates, est vel nemo impedit dolorem quisquam at blanditiis dolor.'
         ],
         [
+            'id' => 2,
+            'slug' => 'title-article-2',
             'title' => 'Title Article 2',
             'author' => 'Daffa Rinaldi',
             'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi et, exercitationem tempora reprehenderit quos corporis rem architecto ipsum tempore? Quasi dolore magnam fuga molestiae sit ratione dolorem ducimus ullam similique.'
         ]
     ]]);
+});
+
+Route::get('/posts/{slug}', function($slug){
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'title-article-1',
+            'title' => 'Title Article 1',
+            'author' => 'Daffa Rinaldi',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic ducimus asperiores laboriosam odit enim aperiam ipsum repellendus quas, corporis, non voluptates, est vel nemo impedit dolorem quisquam at blanditiis dolor.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'title-article-2',
+            'title' => 'Title Article 2',
+            'author' => 'Daffa Rinaldi',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi et, exercitationem tempora reprehenderit quos corporis rem architecto ipsum tempore? Quasi dolore magnam fuga molestiae sit ratione dolorem ducimus ullam similique.'
+        ]
+        ];
+
+    $post = Arr::first($posts, function($post) use ($slug){
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+
 });
 
 Route::get('/contact', function () {
