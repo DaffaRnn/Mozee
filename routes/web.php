9baf2d42c756a06d3e8b1fd31a2fc5a931f0138f
Daffa Rinaldi <daffaxzy890@gmail.com>
2025-05-25 15:13:27 +0700

    implements models to project

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 Route::get('/', function () {
     return view('home', ["title" => "Home"]);
@@ -12,43 +13,12 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ["title" => "Blog", "posts" => [
-        [
-            'id' => 1,
-            'slug' => 'title-article-1',
-            'title' => 'Title Article 1',
-            'author' => 'Daffa Rinaldi',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic ducimus asperiores laboriosam odit enim aperiam ipsum repellendus quas, corporis, non voluptates, est vel nemo impedit dolorem quisquam at blanditiis dolor.'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'title-article-2',
-            'title' => 'Title Article 2',
-            'author' => 'Daffa Rinaldi',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi et, exercitationem tempora reprehenderit quos corporis rem architecto ipsum tempore? Quasi dolore magnam fuga molestiae sit ratione dolorem ducimus ullam similique.'
-        ]
-    ]]);
+    return view('posts', ["title" => "Blog", "posts" => Post::all()]);
 });
 
 Route::get('/posts/{slug}', function($slug){
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'title-article-1',
-            'title' => 'Title Article 1',
-            'author' => 'Daffa Rinaldi',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic ducimus asperiores laboriosam odit enim aperiam ipsum repellendus quas, corporis, non voluptates, est vel nemo impedit dolorem quisquam at blanditiis dolor.'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'title-article-2',
-            'title' => 'Title Article 2',
-            'author' => 'Daffa Rinaldi',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi et, exercitationem tempora reprehenderit quos corporis rem architecto ipsum tempore? Quasi dolore magnam fuga molestiae sit ratione dolorem ducimus ullam similique.'
-        ]
-        ];
-
-    $post = Arr::first($posts, function($post) use ($slug){
+   
+    $post = Arr::first(Post::all(), function($post) use ($slug){
         return $post['slug'] == $slug;
     });
 
