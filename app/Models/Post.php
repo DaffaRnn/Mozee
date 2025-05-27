9ffc071623e51530b895e2aa6f38fe83f406ace3
Daffa Rinaldi <daffaxzy890@gmail.com>
2025-05-27 20:25:48 +0700

    Using eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,23 +1,10 @@
 <?php
+
 namespace App\Models;
 
-class Post{
-    public static function all(){
-        return [
-        [
-            'id' => 1,
-            'slug' => 'title-article-1',
-            'title' => 'Title Article 1',
-            'author' => 'Daffa Rinaldi',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic ducimus asperiores laboriosam odit enim aperiam ipsum repellendus quas, corporis, non voluptates, est vel nemo impedit dolorem quisquam at blanditiis dolor.'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'title-article-2',
-            'title' => 'Title Article 2',
-            'author' => 'Daffa Rinaldi',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi et, exercitationem tempora reprehenderit quos corporis rem architecto ipsum tempore? Quasi dolore magnam fuga molestiae sit ratione dolorem ducimus ullam similique.'
-        ]
-        ];
-    }
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+
+class Post extends Model{
+   
 }
